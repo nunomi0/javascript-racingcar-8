61525563587f8a2e8c22b0a3efd37cb4db8ae845
nunomi0 <ykys1@naver.com>
2025-10-27 22:38:33 +0900

    test(racinggame, output): 우승자 선출 테스트 추가

--- __tests__/RacingGame.test.js
+++ __tests__/RacingGame.test.js
@@ -1,5 +1,6 @@
 import { RacingGame } from "../src/RacingGame.js";
-import * as randomUtils from "../src/utils/randomUtils.js"; // ← 모듈 전체를 가져와서 spyOn 가능하게
+import * as randomUtils from "../src/utils/randomUtils.js";
+import { Car } from "../src/Car.js";
 
 describe("RacingGame 클래스", () => {
   afterEach(() => {
@@ -27,6 +28,40 @@ describe("RacingGame 클래스", () => {
     game.playRound();
     game.cars.forEach((car) => {
       expect(car.position).toBe(0);
+    });
+  });
+
+  describe("우승자 선출", () => {
+    test("가장 멀리 간 자동차가 단독 우승자가 된다.", () => {
+      const game = new RacingGame(["pobi", "woni", "jun"]);
+      game.cars = [
+        new Car("pobi"),
+        new Car("woni"),
+        new Car("jun"),
+      ];
+
+      game.cars[0].position = 2;
+      game.cars[1].position = 5;
+      game.cars[2].position = 3;
+
+      const winners = game.getWinners();
+      expect(winners).toEqual(["woni"]);
+    });
+
+    test("가장 멀리 간 자동차가 여러 대면 공동 우승자가 된다.", () => {
+      const game = new RacingGame(["pobi", "woni", "jun"]);
+      game.cars = [
+        new Car("pobi"),
+        new Car("woni"),
+        new Car("jun"),
+      ];
+
+      game.cars[0].position = 4;
+      game.cars[1].position = 4;
+      game.cars[2].position = 2;
+
+      const winners = game.getWinners();
+      expect(winners).toEqual(["pobi", "woni"]);
     });
   });
 });
--- __tests__/output.test.js
+++ __tests__/output.test.js
@@ -1,9 +1,16 @@
-import { printRoundResult } from "../src/output.js";
+import { printRoundResult, printWinners } from "../src/output.js";
 import { Console } from "@woowacourse/mission-utils";
 
-describe("출력", () => {
+describe("printRoundResult", () => {
+  beforeEach(() => {
+    jest.spyOn(Console, "print").mockImplementation(() => {});
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
   test("자동차의 현재 위치를 '-'로 출력한다.", () => {
-    const spy = jest.spyOn(Console, "print").mockImplementation(() => {});
     const cars = [
       { name: "pobi", position: 2 },
       { name: "crong", position: 3 },
@@ -11,9 +18,30 @@ describe("출력", () => {
 
     printRoundResult(cars);
 
-    expect(spy).toHaveBeenCalledWith("pobi : --");
-    expect(spy).toHaveBeenCalledWith("crong : ---");
-    expect(spy).toHaveBeenCalledWith("");
-    spy.mockRestore();
+    expect(Console.print).toHaveBeenCalledWith("pobi : --");
+    expect(Console.print).toHaveBeenCalledWith("crong : ---");
+    expect(Console.print).toHaveBeenCalledWith("");
+  });
+});
+
+describe("printWinners", () => {
+  beforeEach(() => {
+    jest.spyOn(Console, "print").mockImplementation(() => {});
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  test("우승자가 한 명일 경우 이름을 출력한다.", () => {
+    const winners = ["pobi"];
+    printWinners(winners);
+    expect(Console.print).toHaveBeenCalledWith("최종 우승자 : pobi");
+  });
+
+  test("우승자가 여러 명일 경우 쉼표로 구분해 출력한다.", () => {
+    const winners = ["pobi", "woni", "jun"];
+    printWinners(winners);
+    expect(Console.print).toHaveBeenCalledWith("최종 우승자 : pobi, woni, jun");
   });
 });
