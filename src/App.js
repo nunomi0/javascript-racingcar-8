7d53620327ee4447d4470a8972e7ab31f216b9bc
nunomi0 <ykys1@naver.com>
2025-10-27 19:38:54 +0900

    feat(app): 입력 파싱

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import { readCarNames, readTryCount } from "./input.js";
 
 class App {
   async run() {
-    const carNames = await readCarNames();
+    const carNamesRaw = await readCarNames();
+    const carNames = carNamesRaw.split(",").map((name) => name.trim());
     const tryCount = await readTryCount();
   }
 }
