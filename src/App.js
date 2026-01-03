65d736fa15a0347586a00684690cddd2746611c2
nunomi0 <ykys1@naver.com>
2026-01-03 22:35:59 +0900

    feat(inputview): 자동차 이름, 시도할 횟수 입력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Console } from "@woowacourse/mission-utils";
+import InputView from "./InputView.js";
+
 class App {
-  async run() {}
+  async run() {
+    const carNames = await InputView.readCarNames();
+    const tryCount = await InputView.readTryCount();
+
+    Console.print(carNames);
+    Console.print(tryCount);
+  }
 }
 
 export default App;
---
+++ src/InputView.js
@@ -0,0 +1,11 @@
+import { Console } from "@woowacourse/mission-utils";
+
+export default class InputView {
+  static async readCarNames() {
+    return Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+  }
+
+  static async readTryCount() {
+    return Console.readLineAsync("시도할 횟수는 몇 회인가요?");
+  }
+}
