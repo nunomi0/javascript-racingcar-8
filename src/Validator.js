f692fd1ff757b8ae6a3b23f7ee078229ae67d9cf
nunomi0 <ykys1@naver.com>
2026-01-05 23:14:46 +0900

    docs(readme): 프로그래밍 요구 사항 업데이트

--- src/Validator.js
+++ src/Validator.js
@@ -1,10 +1,4 @@
 export function parseCarNames(input) {
-  /*
-  TODO:
-  이름이 공백인 경우
-  이름이 5자 이하가 아닌 경우
-  */
-
   const carNames = input.split(",");
   for (const carName of carNames){
     if (carName.length>5) throw new Error("[ERROR]");
